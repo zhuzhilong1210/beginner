d8ea3b190f0e22e6851b446462e9a81565ec9196
unknown <zhuzhilong1210@163.com>
2018-12-23 22:24:50 +0800

    使用yml文件

--- src/main/java/com/beginner/App.java
+++ src/main/java/com/beginner/App.java
@@ -3,12 +3,12 @@ package com.beginner;
 import com.beginner.entity.mapper.UserInfoMapper;
 import com.beginner.entity.po.UserInfo;
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 /**
  * Hello world!
@@ -20,16 +20,6 @@ import org.springframework.web.bind.annotation.RestController;
 @MapperScan("com.beginner.entity.mapper")
 public class App 
 {
-    @Autowired
-    private UserInfoMapper userInfoMapper;
-
-    @RequestMapping("/")
-    public String home(){
-        UserInfo userInfo = userInfoMapper.selectByPrimaryKey(1);
-        if(userInfo == null)
-            return "用户不存在！";
-        return  userInfo.getName() ;
-    }
     public static void main( String[] args )
     {
         SpringApplication.run(App.class, args);
--- src/main/java/com/beginner/controller/UserController.java
+++ src/main/java/com/beginner/controller/UserController.java
@@ -5,16 +5,17 @@ import com.beginner.entity.po.UserInfo;
 import com.beginner.exception.BaseException;
 import com.beginner.exception.EnumErrMsg;
 import com.beginner.service.IUserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+
 @RestController
 @RequestMapping("/user")
 public class UserController extends BaseController {
-    @Autowired
+    @Resource
     private IUserService iUserService;
 
     @GetMapping("/userInfo")
